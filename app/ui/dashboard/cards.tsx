bb34115de263ffeff3b1ddb3df5bc57244aeffd3
William Whorton <bwhorton@safetower.com>
2025-09-05 19:48:17 -0400

    Refactor dashboard by introducing `CardWrapper` with dynamic data fetching, Suspense support, and new skeleton loaders; update dependencies and enable PPR experimental feature.

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import {fetchCardData} from "@/app/lib/data";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,22 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  return (
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+    } = await fetchCardData();
+    return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Page"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
@@ -34,11 +39,11 @@ export function Card({
   title,
   value,
   type,
-}: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+}: Readonly<{
+    title: string;
+    value: number | string;
+    type: 'invoices' | 'customers' | 'pending' | 'collected';
+}>) {
   const Icon = iconMap[type];
 
   return (
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-    allowedDevOrigins: ["192.168.50.147"]
+    allowedDevOrigins: ["192.168.50.147"],
+    experimental: {
+        ppr: 'incremental'
+    }
 };
 
 export default nextConfig;

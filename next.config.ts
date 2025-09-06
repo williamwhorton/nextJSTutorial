b0d3513111f596b199171660642f9df4140a3d88
William Whorton <bwhorton@safetower.com>
2025-09-06 18:25:30 -0400

    Remove `experimental.ppr` flag from Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
     allowedDevOrigins: ["192.168.50.147"],
-    experimental: {
-        ppr: 'incremental'
-    }
 };
 
 export default nextConfig;
